ac2950812f5de6d0907a85545212b8a574492ce8
Jerome Salignon <takeo1@hotmail.fr>
2022-11-26 18:42:03 +0100

    trying to fix nextflow tower

--- run_test_datasets.sh
+++ run_test_datasets.sh
@@ -85,6 +85,35 @@ nextflow run jsalignon/cactus -params-file parameters/full_test.yml -profile sin
 
 
 
+nextflow run jsalignon/cactus/scripts/download/download.nf -r main -latest --test_datasets --references --references_dir refs -profile conda --species worm 
+
+# running all tests from scratch
+
+
+homedir=~
+eval homedir=$homedir
+cactus_dir=$homedir/workspace/cactus
+test_dir=$cactus_dir/testing
+
+cpu_nb=47
+memory_size='250G'
+
+cd $test_dir 
+rm -r *
+
+for tools_manager in singularity conda
+do 
+  for species in worm fly human mouse
+  do
+    cd $test_dir
+    mkdir -p $tools_manager/$species
+    cd $test_dir/$tools_manager/$species
+    nextflow run jsalignon/cactus/scripts/download/download.nf -r main -latest --test_datasets --references --references_dir refs -profile $tools_manager --species $species 
+    nextflow run jsalignon/cactus -params-file parameters/full_test.yml --references_dir refs/$species -profile $tools_manager --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --res_dir 'results/almost_full_test'  --split__peak_assignment ['all'] --split__threshold_values [200]
+  done
+done
+
+
 
 nextflow run /home/jersal/workspace/cactus/scripts/download/download.nf --test_datasets --references --species fly --references_dir refs --cactus_dir /home/jersal/workspace/cactus 
 
