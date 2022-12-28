1133462f289aa6638f467e5379c68b1a0d4e27bb
Jerome Salignon <takeo1@hotmail.fr>
2022-12-28 10:57:41 +0100

    updating the script to run the case study

--- test_datasets/running_the_case_study.sh
+++ test_datasets/running_the_case_study.sh
@@ -1,5 +1,10 @@
 
-# # creating the case study datasets
+## notes
+# chromatin state: ENCFF321DGG -> foreskin fibroblast male newborn from donor(s)
+# chromatin states: HiHMM GM12878 -> lymphoblastoid cell line produced from the blood of a female donor with northern and western European ancestry by EBV transformation
+
+
+## creating the case study datasets
 # homedir=~
 # eval homedir=$homedir
 # cactus_dir=$homedir/workspace/cactus
@@ -9,35 +14,54 @@
 # source $create_test_datasets_bin_dir/create_test_datasets_functions.sh
 
 
-# setting up paths
+
+
+## setting up paths
 homedir=~
 eval homedir=$homedir
 cactus_dir=$homedir/workspace/cactus
 test_ds_dir=$cactus_dir/test_datasets
 case_study_dir=$test_ds_dir/application_note
 
-# setting up variables
-cactus_version=0.9.0
+## setting up variables
+# cactus revisions ran: 57a2e7f516d9ac7114f389c0f226ca01ae63db7a
+# cactus_version=0.9.0 ; latest_flag=""
+cactus_version=main ; latest_flag="-latest"
 cpu_nb=47
 memory_size='250G'
-
-
+figshare_version=v4
+references_dir=$cactus_dir/references/$figshare_version
 
 padj_breaks="c( 0.2, 1e-5, 1e-50, 1e-100, 1e-200 )"
 heatmaps_filters_chip="c( 30, 20, 10, T, 2, 'ward.D', T )"
 heatmaps_filters_motifs="c( 30, 20, 10, T, 2, 'ward.D', F )"
 
-cd $case_study_dir/worm
-nextflow run jsalignon/cactus -profile singularity -r $cactus_version -latest -resume --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --species 'worm' --res_dir 'results/2022_12_02' --chromatin_state 'iHMM.M1K16.worm_L3' --chip_ontology 'all' --split__threshold_type 'FDR' --split__threshold_values [1.3] --split__peak_assignment ['all'] --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --padj_breaks__motifs "$padj_breaks" --padj_breaks__CHIP "$padj_breaks" --padj_breaks__chrom_states "$padj_breaks"
 
-cd $case_study_dir/human
+## running Cactus 
+# the parameters after the line break are the shared parameters
 
-# CHIP ontology: 'all'
-# chromatin state: ENCFF321DGG -> foreskin fibroblast male newborn from donor(s)
-nextflow run jsalignon/cactus -profile singularity -r $cactus_version -latest -resume --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --species 'human' --res_dir 'results/2022_12_02__run_1' --chromatin_state 'ENCFF321DGG' --chip_ontology 'all' --split__threshold_type 'FDR' --split__threshold_values [1.3] --split__peak_assignment ['all'] --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --padj_breaks__motifs "$padj_breaks" --padj_breaks__CHIP "$padj_breaks" --padj_breaks__chrom_states "$padj_breaks"
+species=worm ; cd $case_study_dir/$species
+nextflow run jsalignon/cactus --res_dir 'results/2022_12_21__run_1' --chromatin_state 'iHMM.M1K16.worm_L3' --chip_ontology 'all' \
+--species $species -profile singularity -r $cactus_version $latest_flag -resume --references_dir $references_dir --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --common_padj_breaks "$padj_breaks" --common__heatmaps_ggplot "c( 12, 12, 12 )" 
 
-# CHIP ontology: 'cell_type.fibroblast'
-# chromatin states: HiHMM GM12878 -> lymphoblastoid cell line produced from the blood of a female donor with northern and western European ancestry by EBV transformation
-nextflow run jsalignon/cactus -profile singularity -r $cactus_version -latest -resume --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --species 'human' --res_dir 'results/2022_12_02__run_2' --chromatin_state 'iHMM.M1K16.human_GM' --chip_ontology 'cell_type.fibroblast' --split__threshold_type 'FDR' --split__threshold_values [1.3] --split__peak_assignment ['all'] --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --padj_breaks__motifs "$padj_breaks" --padj_breaks__CHIP "$padj_breaks" --padj_breaks__chrom_states "$padj_breaks"
+species=worm ; cd $case_study_dir/$species
+nextflow run jsalignon/cactus --res_dir 'results/2022_12_21__run_2' --chromatin_state 'iHMM.M1K16.worm_L3' --split__threshold_values [4] --do_genes_self_enrichment false --do_chip_enrichment false --do_motif_enrichment false --do_chrom_state_enrichment false --do_func_anno_enrichment false \
+--species $species -profile singularity -r $cactus_version $latest_flag -resume --references_dir $references_dir --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --common_padj_breaks "$padj_breaks" --common__heatmaps_ggplot "c( 12, 12, 12 )" 
+
+species=human ; cd $case_study_dir/$species
+nextflow run jsalignon/cactus --res_dir 'results/2022_12_21__run_1' --chromatin_state 'ENCFF321DGG' --chip_ontology 'all' \
+--species $species -profile singularity -r $cactus_version $latest_flag -resume --references_dir $references_dir --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --common_padj_breaks "$padj_breaks" --common__heatmaps_ggplot "c( 12, 12, 12 )" 
+
+
+species=human ; cd $case_study_dir/$species 
+nextflow run jsalignon/cactus --res_dir 'results/2022_12_21__run_2' --chromatin_state 'iHMM.M1K16.human_GM' --chip_ontology 'cell_type.fibroblast' \
+--species $species -profile singularity -r $cactus_version $latest_flag -resume --references_dir $references_dir --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --common_padj_breaks "$padj_breaks" --common__heatmaps_ggplot "c( 12, 12, 12 )" 
+
+
+## repeating the peak-self analysis with a lower FDR cutoff
+species=human ; cd $case_study_dir/$species
+nextflow run jsalignon/cactus --res_dir 'results/2022_12_21__run_3' --chromatin_state 'iHMM.M1K16.human_GM' --split__threshold_values [4] --do_genes_self_enrichment false --do_chip_enrichment false --do_motif_enrichment false --do_chrom_state_enrichment false --do_func_anno_enrichment false \
+--species $species -profile singularity -r $cactus_version $latest_flag -resume --references_dir $references_dir --executor_local_cpus $cpu_nb --executor_local_memory $memory_size --heatmaps_filter__CHIP "$heatmaps_filters_chip" --heatmaps_filter__motifs "$heatmaps_filters_motifs" --common_padj_breaks "$padj_breaks" --common__heatmaps_ggplot "c( 12, 12, 12 )" 
+
 
 
