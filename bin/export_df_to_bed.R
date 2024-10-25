d951e3fba84acf7fc0f69c9e33df12702f86d93a
Jerome Salignon <takeo1@hotmail.fr>
2024-10-25 17:14:45 +0200

    tiny edit to fix a bug (still the same)

--- bin/export_df_to_bed.R
+++ bin/export_df_to_bed.R
@@ -5,5 +5,7 @@ export_df_to_bed <- function(df, filename){
   df$chr = paste0('chr', df$chr)
   df$start = df$start - 1
   df$start[df$start < 0] = 0
+  df$start %<>% as.integer
+  df$end   %<>% as.integer
   write.table(df, file = filename, quote = F, sep = '\t', row.names = F, col.names = F)
 }
